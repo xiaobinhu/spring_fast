29a338bb70481212a266ae26eb4c20a50d900808
saber <2238657692@qq.com>
2018-11-26 08:59:22 +0800

    啊啊啊啊

---
+++ src/test/java/com/baizhi/spring_fast/SpringFastApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.spring_fast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFastApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
